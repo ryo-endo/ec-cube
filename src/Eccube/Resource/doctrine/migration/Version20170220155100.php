e888c705037d54f3253126ab3d66a61de689e5ee
lqdung <lqdung@lockon-vn.com>
2017-02-21 09:11:15 +0700

    [FIX] Fix migration.

--- src/Eccube/Resource/doctrine/migration/Version20170220155100.php
+++ src/Eccube/Resource/doctrine/migration/Version20170220155100.php
@@ -4,6 +4,9 @@ namespace DoctrineMigrations;
 
 use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
+use Doctrine\ORM\EntityManager;
+use Eccube\Application;
+use Eccube\Entity\PageLayout;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
@@ -15,11 +18,22 @@ class Version20170220155100 extends AbstractMigration
      */
     public function up(Schema $schema)
     {
-        $this->addSql("INSERT INTO dtb_page_layout (device_type_id, page_name, url, file_name, edit_flg, author, description, keyword, update_url, create_date, update_date, meta_robots) VALUES (10, 'お問い合わせ(確認ページ)', 'contact_confirm', 'Contact/confirm', 2, NULL, NULL, NULL, NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL);");
+        $app = Application::getInstance();
+        /** @var EntityManager $em */
+        $em = $app["orm.em"];
 
-        if ($this->connection->getDatabasePlatform()->getName() == "postgresql") {
-            $this->addSql("SELECT setval('dtb_page_layout_page_id_seq', 32);");
-        }
+        $DeviceType = $app['eccube.repository.master.device_type']->find(10);
+
+        $PageLayout = new PageLayout();
+        $PageLayout
+            ->setDeviceType($DeviceType)
+            ->setName('お問い合わせ(確認ページ)')
+            ->setUrl('contact_confirm')
+            ->setFileName('Contact/confirm')
+            ->setEditFlg(2)
+            ->setMetaRobots('noindex');
+        $em->persist($PageLayout);
+        $em->flush();
     }
 
     /**
